0b49861a31b38aa3a6ab295cadff136b8b871922
XirevGH <74151509+XirevGH@users.noreply.github.com>
2025-04-14 14:29:53 +0200

    Started building AI

---
+++ Source/CoolGang/EnemyAIController.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyAIController.h"
+#include "PlayerCharacter.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void AEnemyAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+	if (AIBehavior != nullptr)
+	{
+		RunBehaviorTree(AIBehavior);
+
+		APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+	}
+}
+
+bool AEnemyAIController::IsDead() const
+{
+	APlayerCharacter* ControlledCharacter = Cast<APlayerCharacter>(GetPawn());
+
+	if (ControlledCharacter != nullptr)
+	{
+		return ControlledCharacter->IsDead();
+	}
+	
+	return true;
+}
