33692d05e478933cecf3c404134983ade5406a35
XirevGH <74151509+XirevGH@users.noreply.github.com>
2025-04-15 10:21:28 +0200

    Added line of sight for enemy

--- Source/CoolGang/BTService_PlayerInLineOfSight.cpp
+++ Source/CoolGang/BTService_PlayerInLineOfSight.cpp
@@ -1,5 +1,34 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BTService_PlayerInLineOfSight.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
 
+UBTService_PlayerInLineOfSight::UBTService_PlayerInLineOfSight()
+{
+	NodeName = "Player In Line Of Sight";
+}
+
+void UBTService_PlayerInLineOfSight::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	
+	if (OwnerComp.GetAIOwner() == nullptr)
+	{
+		return;
+	}
+	
+	if (PlayerPawn == nullptr)
+	{
+		return;
+	}
+	
+	if (OwnerComp.GetAIOwner()->LineOfSightTo(PlayerPawn))
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsObject(GetSelectedBlackboardKey(), PlayerPawn);
+	}
+	else
+	{
+		OwnerComp.GetBlackboardComponent()->ClearValue(GetSelectedBlackboardKey());
+	}
+}
