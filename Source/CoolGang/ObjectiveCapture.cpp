f537e39ae4b391565a7be42581b33a90a3da7d59
ChrisPil03 <144431149+ChrisPil03@users.noreply.github.com>
2025-04-18 20:48:30 +0200

    Objective parent class + new remade objectives + Interactable object
    Made a parent class for all objectives. Added new remade Objectivedownload and ObjectivePressButtons. Made a class InteractableObject, can set a function to be called when it is interacted with. Created an ObjectiveSpawnPoint blueprint

---
+++ Source/CoolGang/ObjectiveCapture.cpp
@@ -0,0 +1,132 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ObjectiveCapture.h"
+#include "PlayerCharacter.h"
+#include "Components/SphereComponent.h"
+
+AObjectiveCapture::AObjectiveCapture()
+{
+	CaptureRadius = 600.f;
+	PlayerTag = "Player";
+	PlayerInZone = nullptr;
+	SetIsTimeBased(true);
+	
+	SphereTrigger = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Trigger Component"));
+	SphereTrigger->InitSphereRadius(CaptureRadius);
+	SphereTrigger->SetCollisionProfileName(TEXT("Trigger"));
+	RootComponent = SphereTrigger;
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(SphereTrigger);
+}
+
+void AObjectiveCapture::BeginPlay()
+{
+	Super::BeginPlay();
+	SphereTrigger->OnComponentBeginOverlap.AddDynamic(this, &AObjectiveCapture::OnSphereBeginOverlap);
+	SphereTrigger->OnComponentEndOverlap.AddDynamic(this, &AObjectiveCapture::OnSphereEndOverlap);
+}
+
+void AObjectiveCapture::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (GetIsComplete())
+	{
+		return;
+	}
+	
+	if (GetIsInProgress())
+	{
+		IncreaseObjectiveProgress(DeltaTime);
+	}
+	else if (GetIsAborting())
+	{
+		DecreaseObjectiveProgress(DeltaTime);
+	}
+}
+
+void AObjectiveCapture::Interact(AActor* Interactor)
+{
+	if (GetIsNotStarted())
+	{
+		StartObjective();
+	}
+}
+
+void AObjectiveCapture::StartObjective()
+{
+	Super::StartObjective();
+	SpawnCaptureZone();
+}
+
+void AObjectiveCapture::ResetObjective()
+{
+	Super::ResetObjective();
+	DestroyCaptureZone();
+}
+
+void AObjectiveCapture::IncreaseObjectiveProgress(float const DeltaTime)
+{
+	Super::IncreaseObjectiveProgress(DeltaTime);
+
+	if (GetObjectiveProgress() == Complete)
+	{
+		CompleteObjective();
+	}
+}
+
+void AObjectiveCapture::DecreaseObjectiveProgress(float const DeltaTime)
+{
+	Super::DecreaseObjectiveProgress(DeltaTime);
+
+	if (GetObjectiveProgress() == ZeroCompletion)
+	{
+		ResetObjective();
+	}
+}
+
+void AObjectiveCapture::UpdateObjectiveProgress(float const NewProgress)
+{
+	Super::UpdateObjectiveProgress(NewProgress);
+	UpdateCaptureZoneSize();
+}
+
+void AObjectiveCapture::OnSphereBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult)
+{
+	if (!OtherActor || OtherActor == this) return;
+	
+	if (APlayerCharacter* Player = Cast<APlayerCharacter>(OtherActor))
+	{
+		PlayerInZone = Player;
+	}
+
+	if (GetIsAborting())
+	{
+		SetObjectiveState(EObjectiveState::InProgress);
+	}
+}
+
+void AObjectiveCapture::OnSphereEndOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex)
+{
+	if (!OtherActor || OtherActor == this) return;
+	
+	if (OtherActor == PlayerInZone)
+	{
+		PlayerInZone = nullptr;
+	}
+
+	if (GetIsInProgress())
+	{
+		SetObjectiveState(EObjectiveState::Aborting);
+	}
+}
---
+++ Source/CoolGang/ObjectiveTimedTasks.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ObjectiveBase.h"
+#include "ObjectiveTimedTasks.generated.h"
+
+class AInteractableObject;
+class AObjectiveButton;
+
+UCLASS()
+class COOLGANG_API AObjectiveTimedTasks : public AObjectiveBase
+{
+	GENERATED_BODY()
+
+public:
+	AObjectiveTimedTasks();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void ResetObjective() override;
+	virtual void IncreaseObjectiveProgress(float const DeltaTime) override;
+	virtual void CompleteObjective() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UFUNCTION()
+	void RegisterInteraction(AInteractableObject* InteractableObject);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess="true"), Category="Objective")
+	TArray<AInteractableObject*> AllInteractableObjects;
+
+	UPROPERTY(VisibleAnywhere)
+	int InteractedTasks;
+};
