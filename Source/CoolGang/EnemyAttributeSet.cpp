c1be06cbe5676e72fcca28622286ab898ce06e67
asawinSit <144443633+asawinSit@users.noreply.github.com>
2025-04-30 11:15:06 +0200

    hit fix damage

--- Source/CoolGang/EnemyAttributeSet.cpp
+++ Source/CoolGang/EnemyAttributeSet.cpp
@@ -37,9 +37,9 @@ void UEnemyAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallb
 			if (CurrentHealth > 0.0f)
 			{
 				UE_LOG(LogTemp, Error, TEXT("Enemy health being subtract by %f"), Magnitude);
-				float NewHealth = FMath::Clamp(CurrentHealth - Magnitude, 0.0f, GetMaxHealth());
-				SetHealth(NewHealth);
-				UE_LOG(LogTemp, Error, TEXT("Enemy health After being subtracted: %f"), GetHealth());
+				//float NewHealth = FMath::Clamp(CurrentHealth - Magnitude, 0.0f, GetMaxHealth());
+				//SetHealth(NewHealth);
+				//UE_LOG(LogTemp, Error, TEXT("Enemy health After being subtracted: %f"), GetHealth());
 			}
 		}
 		
