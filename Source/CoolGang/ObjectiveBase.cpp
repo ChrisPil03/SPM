92409f105cef1e4d5a0ee1bb01972e06e2e2f160
ChrisPil03 <144431149+ChrisPil03@users.noreply.github.com>
2025-05-13 18:51:48 +0200

    Failed objectives can activate again

--- Source/CoolGang/ObjectiveBase.cpp
+++ Source/CoolGang/ObjectiveBase.cpp
@@ -17,7 +17,7 @@ AObjectiveBase::AObjectiveBase() :
 	Progress(0.f),
 	SystemIntegrity(nullptr),
 	BaseIntegrityDamage(100.f),
-	ObjectiveFailedIntegrityChunkDamage(25000.f),
+	ObjectiveFailedIntegrityChunkDamage(20000.f),
 	ActivatedMessage("MALFUNCTION DETECTED"),
 	StartedMessage("OBJECTIVE STARTED"),
 	CompletedMessage("OBJECTIVE COMPLETED"),
