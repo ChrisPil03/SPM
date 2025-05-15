9d6882949786f0388d1783be62af095703810cda
bondhemlig <stenhammarelias@gmail.com>
2025-05-15 18:53:14 +0200

    nothing added to array

--- Source/CoolGang/FileHelperLibrary.cpp
+++ Source/CoolGang/FileHelperLibrary.cpp
@@ -8,7 +8,7 @@
 TArray<FString> UFileHelperLibrary::GetAllFilesInDirectory(const FString& Directory, const FString& FileExtension)
 {
 	TArray<FString> FileNames;
-	UE_LOG(LogTemp, Warning, TEXT("Looking for files! 11"));
+	UE_LOG(LogTemp, Warning, TEXT("Looking for files!"));
 
 	// Ensure the directory path is valid
 	FString FullPath = FPaths::ConvertRelativePathToFull(Directory);
@@ -17,10 +17,39 @@ TArray<FString> UFileHelperLibrary::GetAllFilesInDirectory(const FString& Direct
 		UE_LOG(LogTemp, Warning, TEXT("Directory does not exist: %s"), *FullPath);
 		return FileNames;
 	}
+
 	UE_LOG(LogTemp, Warning, TEXT("Looking for files!"));
+
 	// Use IFileManager to find files
 	IFileManager& FileManager = IFileManager::Get();
 	FileManager.FindFiles(FileNames, *FString::Printf(TEXT("%s/%s"), *FullPath, *FileExtension), true, false);
-	UE_LOG(LogTemp, Warning, TEXT("Found some files!"));
-	return FileNames;
+
+	// Print the number of files found
+	UE_LOG(LogTemp, Warning, TEXT("Found %d files."), FileNames.Num());
+
+	// Prepare array for stripped names
+	TArray<FString> StrippedFileNames;
+
+	for (const FString& FileName : FileNames)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("File: %s"), *FileName);
+
+		// If the file name ends with ".sav", remove it
+		if (FileName.EndsWith(TEXT(".sav")))
+		{
+			StrippedFileNames.Add(FileName.LeftChop(4));
+		}
+		else
+		{
+			StrippedFileNames.Add(FileName);
+		}
+	}
+
+	// Optionally print the stripped names
+	for (const FString& Stripped : StrippedFileNames)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Stripped File Name: %s"), *Stripped);
+	}
+
+	return StrippedFileNames;
 }
