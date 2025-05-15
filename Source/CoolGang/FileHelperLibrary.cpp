74aa1f570330bfd6745579918c28b4b1ed45a6a9
bondhemlig <stenhammarelias@gmail.com>
2025-05-15 17:59:51 +0200

    Update Main Menu
    Leaderboard button (causes infinite loop, debugging in the works)

--- Source/CoolGang/FileHelperLibrary.cpp
+++ Source/CoolGang/FileHelperLibrary.cpp
@@ -8,6 +8,7 @@
 TArray<FString> UFileHelperLibrary::GetAllFilesInDirectory(const FString& Directory, const FString& FileExtension)
 {
 	TArray<FString> FileNames;
+	UE_LOG(LogTemp, Warning, TEXT("Looking for files! 11"));
 
 	// Ensure the directory path is valid
 	FString FullPath = FPaths::ConvertRelativePathToFull(Directory);
@@ -16,10 +17,10 @@ TArray<FString> UFileHelperLibrary::GetAllFilesInDirectory(const FString& Direct
 		UE_LOG(LogTemp, Warning, TEXT("Directory does not exist: %s"), *FullPath);
 		return FileNames;
 	}
-
+	UE_LOG(LogTemp, Warning, TEXT("Looking for files!"));
 	// Use IFileManager to find files
 	IFileManager& FileManager = IFileManager::Get();
 	FileManager.FindFiles(FileNames, *FString::Printf(TEXT("%s/%s"), *FullPath, *FileExtension), true, false);
-
+	UE_LOG(LogTemp, Warning, TEXT("Found some files!"));
 	return FileNames;
 }
