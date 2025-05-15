9d3879693cc59fc03ad4ad86c013853b26ac652c
asawinSit <asawin.sitthi@outlook.com>
2025-05-16 01:23:58 +0200

    Revert "Merge pull request #7 from ChrisPil03/Elias"
    This reverts commit 093958fb61e3d22086295a97cb503600a26fd9c8, reversing
    changes made to 38568bd60b3c8e69496f4a9e3767c1f0832eb341.

--- Source/CoolGang/FileHelperLibrary.cpp
+++ Source/CoolGang/FileHelperLibrary.cpp
@@ -8,7 +8,7 @@
 TArray<FString> UFileHelperLibrary::GetAllFilesInDirectory(const FString& Directory, const FString& FileExtension)
 {
 	TArray<FString> FileNames;
-	UE_LOG(LogTemp, Warning, TEXT("Looking for files!"));
+	UE_LOG(LogTemp, Warning, TEXT("Looking for files! 11"));
 
 	// Ensure the directory path is valid
 	FString FullPath = FPaths::ConvertRelativePathToFull(Directory);
@@ -17,39 +17,10 @@ TArray<FString> UFileHelperLibrary::GetAllFilesInDirectory(const FString& Direct
 		UE_LOG(LogTemp, Warning, TEXT("Directory does not exist: %s"), *FullPath);
 		return FileNames;
 	}
-
 	UE_LOG(LogTemp, Warning, TEXT("Looking for files!"));
-
 	// Use IFileManager to find files
 	IFileManager& FileManager = IFileManager::Get();
 	FileManager.FindFiles(FileNames, *FString::Printf(TEXT("%s/%s"), *FullPath, *FileExtension), true, false);
-
-	// Print the number of files found
-	UE_LOG(LogTemp, Warning, TEXT("Found %d files."), FileNames.Num());
-
-	// Prepare array for stripped names
-	TArray<FString> StrippedFileNames;
-
-	for (const FString& FileName : FileNames)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("File: %s"), *FileName);
-
-		// If the file name ends with ".sav", remove it
-		if (FileName.EndsWith(TEXT(".sav")))
-		{
-			StrippedFileNames.Add(FileName.LeftChop(4));
-		}
-		else
-		{
-			StrippedFileNames.Add(FileName);
-		}
-	}
-
-	// Optionally print the stripped names
-	for (const FString& Stripped : StrippedFileNames)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Stripped File Name: %s"), *Stripped);
-	}
-
-	return StrippedFileNames;
+	UE_LOG(LogTemp, Warning, TEXT("Found some files!"));
+	return FileNames;
 }
