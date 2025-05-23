202b5de1afb342241d9db66dd8bce6487a18ada5
ChrisPil03 <144431149+ChrisPil03@users.noreply.github.com>
2025-05-23 12:12:45 +0200

    Character movement and shotgun balancing

--- Source/CoolGang/EnemyAI.cpp
+++ Source/CoolGang/EnemyAI.cpp
@@ -244,7 +244,6 @@ void AEnemyAI::AttackObjective(AObjectiveBase* Objective)
 	
 	if (!bChangedToTargetPlayer && AliveEnemies.Contains(this))
 	{
-		AttackRange = 2000.f;
 		CurrentTarget = Objective;
 		bChangedToTargetPlayer = true;
 	}
@@ -261,7 +260,6 @@ void AEnemyAI::GiveScore()
 
 void AEnemyAI::AttackPlayer(AObjectiveBase* Objective)
 {
-	AttackRange = 300.f;
 	CurrentTarget = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
 }
 
