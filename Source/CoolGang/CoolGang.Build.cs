ce8480cdc2fbf6d31cdfedc3f6deb63edb579f7f
XirevGH <74151509+XirevGH@users.noreply.github.com>
2025-06-08 22:59:47 +0200

    Added movie at the start of the game and loading screen between the levels with a global fade

--- Source/CoolGang/CoolGang.Build.cs
+++ Source/CoolGang/CoolGang.Build.cs
@@ -15,16 +15,17 @@ public class CoolGang : ModuleRules
 			"GameplayTasks",
 			"GameplayTags",
 			"NavigationSystem",
-			"Slate",       // Slate is a low-level UI framework in Unreal Engine 
-			"SlateCore",   
+			"Slate",
+			"SlateCore",
+			"MoviePlayer"
 		});
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
